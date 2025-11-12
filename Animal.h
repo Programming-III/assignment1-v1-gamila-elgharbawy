54e339ac1e32ae434272d022664d1b6512555fe0
gamila-elgharbawy <gamila.elgharbawy@student.giu-uni.de>
2025-11-12 15:26:46 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -1,11 +1,28 @@
 #ifndef ANIMAL_H
 #define ANIMAL_H
-
-#include <string>
+#include<iostream>
+#include<string>
 using namespace std;
+class Animal {
 
-//define animal class here 
-
+private:
+	string name;
+	int age;
+	bool isHungry;
+public:
+	Animal();
+	Animal(string n, int a, bool isHungry)
+		~Animal();
+	void setName(string n);
+	void setAge(int a);
+	void setIsHungry(bool isHungry);
+	string getName();
+	int getAge();
+	bool getIsHungry();
+	void display();
+	void feed();
 
 
 #endif
+
+}; 
