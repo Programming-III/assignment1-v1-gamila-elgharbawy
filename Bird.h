be7a5e7ae62fdd7da5e0da3cceddd764dbf08985
gamila-elgharbawy <gamila.elgharbawy@student.giu-uni.de>
2025-11-12 15:27:06 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -1,14 +1,21 @@
 #ifndef BIRD_H
 #define BIRD_H
+#include<iostream>
+#include<string>
+#include"Animal.h"
+using namespace std;
+class Bird:public Animal {
 
-#include "Animal.h"
-
-//define class bird here 
-
-
-
-
-
-
+private:
+	float wingSpan;
+public:
+	Bird();
+	Bird(string n, int a, bool isHungry,float wingSpan);
+		~Bird();
+	void setwingSpan(float wingSpan);
+	string getwingSpan();
 
 #endif
+
+};
+
