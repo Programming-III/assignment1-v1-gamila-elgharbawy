23e17863ab6eeebaf8a5bc1102dededdf5f639cb
gamila-elgharbawy <gamila.elgharbawy@student.giu-uni.de>
2025-11-12 15:27:23 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -1,13 +1,21 @@
 #ifndef MAMMAL_H
 #define MAMMAL_H
+#include<iostream>
+#include<string>
+#include"Animal.h"
+using namespace std;
+class Mammal :public Animal {
 
-#include "Animal.h"
-
-//define mammal class here
-
-
-
-
-
+private:
+	string furColour;
+public:
+	Mammal();
+	Mammal(string n, int a, bool isHungry,string furColour);
+		~Mammal();
+	void setName(string furColour);
+	string getfurColour();
 
 #endif
+
+};
+
